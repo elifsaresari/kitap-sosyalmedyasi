81be7a8205b217ddd5fbe59aa9ab13403aa1f827
elifsaresari <elsasa05@gmail.com>
2021-11-08 20:32:05 +0300

    add login page

---
+++ Controllers/HakkındaController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _1000Kitap.Controllers
+{
+    public class HakkındaController : Controller
+    {
+        // GET: Hakkında
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult kullanim_sartlari()
+        {
+            return View();
+        }
+
+        public ActionResult gizlilik_politikası()
+        {
+            return View();
+        }
+        public ActionResult topluluk_kuralları()
+        {
+            return View();
+        }
+
+        public ActionResult reklam_verin()
+        {
+            return View();
+        }
+        public ActionResult iletisim()
+        {
+            return View();
+        }
+        public ActionResult dizinler()
+        {
+            return View();
+        }
+
+    }
+}
