952069d45bd5a30faa892ed813e550101178455a
elifsaresari <elsasa05@gmail.com>
2021-11-22 16:30:34 +0300

    Layout2.css added

---
+++ Controllers/KesfetController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _1000Kitap.Controllers
+{
+    public class KesfetController : Controller
+    {
+        // GET: Kesfet
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Enyeniler()
+        {
+            return View();
+        }
+        public ActionResult Onecıkanlar()
+        {
+            return View();
+        } 
+        public ActionResult Ilgigorenler()
+        {
+            return View();
+        }
+
+
+
+    }
+}
---
+++ Controllers/_Layout2Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _1000Kitap.Controllers
+{
+    public class _Layout2Controller : Controller
+    {
+        // GET: _Layout2
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
