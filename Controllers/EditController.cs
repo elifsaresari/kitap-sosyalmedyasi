d3fc31acb4d2f9c858c7f811cb30991d8b2a4516
elifsaresari <elsasa05@gmail.com>
2022-01-01 14:33:12 +0300

    added personel profile page

---
+++ Controllers/EditController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _1000Kitap.Controllers
+{
+    public class EditController : Controller
+    {
+        // GET: Edi
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
